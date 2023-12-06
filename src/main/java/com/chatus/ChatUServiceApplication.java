f0895d8358ecd18f7661e35f96edef86329fa91f
Radu-Augustin Vele <vele.ne.radu@student.utcluj.ro>
2023-12-06 17:45:35 +0200

    initial commit

---
+++ src/main/java/com/chatus/ChatUServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatUServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatUServiceApplication.class, args);
+	}
+
+}
